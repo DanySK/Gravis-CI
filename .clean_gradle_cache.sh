af1a75dfcc3d01f14f4ab3f612fe447a98534e19
Danilo Pianini <danilo.pianini@unibo.it>
2019-07-04 14:51:05 +0200

    Fix error in bash array usage

--- .clean_gradle_cache.sh
+++ .clean_gradle_cache.sh
@@ -3,13 +3,13 @@ rm -fr $HOME/.gradle/caches/*/plugin-resolution
 rm -fr $HOME/.gradle/caches/*/workerMain/
 rm -f $HOME/.gradle/caches/*/fileHashes/*.bin
 PATTERNS=(
-    '*.lock',
-    'buildSrc.jar',
-    'cache.properties',
-    'file-access.bin',
+    '*.lock'
+    'buildSrc.jar'
+    'cache.properties'
+    'file-access.bin'
     'user-id.txt'
 )
-for PATTERN in PATTERNS
+for PATTERN in ${PATTERNS[@]}
 do
     find "${HOME}/.gradle/caches/" -name "${PATTERN}" -type f -delete
 done
