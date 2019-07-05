e6136688012221d69cfd874e9270f8a4520b3a5f
Danilo Pianini <danilo.pianini@unibo.it>
2019-07-05 14:46:33 +0200

    Fix error in if

--- .win-autoclrf-workaround.sh
+++ .win-autoclrf-workaround.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ $TRAVIS_OS_NAME = "windows"]
-do
+then
     echo working around bad Windows autoclrf setting on Travis
     git config --global core.autocrlf false
     git config core.autocrlf false
     git rm --cached -r .
     git reset --hard
-done
+fi
