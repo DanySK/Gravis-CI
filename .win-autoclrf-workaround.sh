95764c9be84a6577a8843003cdf90e99f1eb4f45
Danilo Pianini <danilo.pianini@unibo.it>
2019-07-05 15:14:57 +0200

    Add debug prints

--- .win-autoclrf-workaround.sh
+++ .win-autoclrf-workaround.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-if [ $TRAVIS_OS_NAME = "windows" ]
+echo Work around script for autoclrf setting on Windows under Travis CI
+if [ $TRAVIS_OS_NAME = 'windows' ]
 then
     echo working around bad Windows autoclrf setting on Travis
     git config --global core.autocrlf false
     git config core.autocrlf false
     git rm --cached -r .
     git reset --hard
+else
+    echo Environment $TRAVIS_OS_NAME does not need any action
 fi
