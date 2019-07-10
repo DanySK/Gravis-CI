a0d65cdaa20b05ebd9b3a98a6c54e16ac191040b
Danilo Pianini <danilo.pianini@unibo.it>
2019-07-10 12:41:41 +0200

    Update .disable-windows-defender.sh

--- .disable-windows-defender.sh
+++ .disable-windows-defender.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Note: I'm not sure this works.
-echo This is an attempt at disabling Windows Defender. I'm not sure it works.
-powershell -ExecutionPolicy Bypass -Command "Start-Process powershell -Verb RunAs -ArgumentList '-ExecutionPolicy Bypass -Command \"Set-MpPreference -DisableRealtimeMonitoring \$true\"'"
-powershell -ExecutionPolicy Bypass -Command 'Get-MpPreference > wd-status.log'
-cat wd-status.log
+if [ $TRAVIS_OS_NAME = 'windows' ]
+then
+  echo "This is an attempt at disabling Windows Defender. I'm not sure it works."
+  powershell -ExecutionPolicy Bypass -Command "Start-Process powershell -Verb RunAs -ArgumentList '-ExecutionPolicy Bypass -Command \"Set-MpPreference -DisableRealtimeMonitoring \$true\"'"
+  powershell -ExecutionPolicy Bypass -Command 'Get-MpPreference > wd-status.log'
+  cat wd-status.log
+fi
