5e23918e0c7068a148b238dd148f6b00e4139047
Danilo Pianini <danilo.pianini@unibo.it>
2019-07-15 15:25:49 +0200

    Add script for installing arbitrary Maven versions

---
+++ .install-maven.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+echo "Installing Maven ${MAVEN_VERSION} on ${TRAVIS_OS_NAME}"
+MAVEN_INSTALL_FOLDER="${HOME}"
+echo "Creating ${MAVEN_INSTALL_FOLDER}"
+mkdir -p $MAVEN_INSTALL_FOLDER
+echo "Downloading Maven ${MAVEN_VERSION} from ${MAVEN_DOWNLOAD_URL} into ${MAVEN_INSTALL_FOLDER}"
+MAVEN_TARBALL="apache-maven-${MAVEN_VERSION}-bin.tar.gz"
+MAVEN_DOWNLOAD_URL="http://mirror.nohup.it/apache/maven/maven-3/${MAVEN_VERSION}/binaries/${MAVEN_TARBALL}.tar.gz"
+MAVEN_INSTALLER_LOCATION="${MAVEN_INSTALL_FOLDER}/apache-maven-${MAVEN_VERSION}-bin.tar.gz"
+curl -sL $MAVEN_DOWNLOAD_URL --output $MAVEN_INSTALLER_LOCATION
+echo "Unpacking ${MAVEN_INSTALLER_LOCATION}..."
+tar xzvf $MAVEN_INSTALLER_LOCATION --directory $MAVEN_INSTALL_FOLDER
+echo "Updating PATH..."
+export PATH="$MAVEN_INSTALL_FOLDER/apache-maven-${MAVEN_VERSION}/bin:$PATH"
+echo "Testing maven version"
+mvn -v
+set +e
