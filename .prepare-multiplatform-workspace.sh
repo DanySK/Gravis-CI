e3fa8beef49daffdb70ed2ce31a351e2c64d4fce
Danilo Pianini <danilo.pianini@unibo.it>
2019-08-27 12:54:02 +0200

    Experimental multiplatform workspace support

---
+++ .prepare-multiplatform-workspace.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+$CACHED_BUILD_FOLDER=${1:$TRAVIS_BUILD_DIR/build}
+echo "saving data of $CACHED_BUILD_FOLDER"
+# Prepare replacement
+export TOREPLACE=$(echo $HOME | sed "s:/:\\\\/:g")
+echo $TO_REPLACE
+# Prepare workspace for Windows
+WIN_HOME=/c/Users/$USER
+OSX_HOME=/Users/$USER
+WIN_FOLDER=$(echo "$CACHED_BUILD_FOLDER" | sed "s:$TO_REPLACE*:$WIN_HOME:")
+echo destination copy folder for Windows: $WIN_FOLDER
+OSX_FOLDER=$(echo "$CACHED_BUILD_FOLDER" | sed "s:$TO_REPLACE*:$OSX_HOME:")
+echo destination copy folder for OSX $OSX_FOLDER
+- sudo mkdir -p $WIN_FOLDER
+- sudo mkdir -p $OSX_FOLDER
+- sudo chown $USER $WIN_FOLDER
+- sudo chown $USER $OSX_FOLDER
+- cp -R "$CACHED_BUILD_FOLDER"/* $WIN_FOLDER
+- cp -R "$CACHED_BUILD_FOLDER"/* $OSX_FOLDER
