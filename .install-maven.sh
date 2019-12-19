20b4dbb79572a8fa5cfb3e2dee995bd29e8d3b4e
Danilo Pianini <danilo.pianini@unibo.it>
2019-12-19 12:14:40 +0100

    Try to fetch the latest maven from the mirror

--- .install-maven.sh
+++ .install-maven.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 set -e
-MAVEN_VERSION=${MAVEN_VERSION:-3.6.1}
+MIRROR="https://archive.apache.org/dist/maven/maven-3/"
+echo "Fetching the latest available Maven from $MIRROR"
+LATEST_MAVEN=$( curl $MIRROR | python -c "
+import re
+import sys
+versions = [
+  match
+  for line in sys.stdin.readlines()
+  for match in re.findall(r'.*href=\"(\d+\.\d+\.\d+)\/\">', line)
+]
+print(max(versions))
+" )
+echo "Latest available version is $LATEST_MAVEN"
+MAVEN_VERSION=${MAVEN_VERSION:-$LATEST_MAVEN}
 echo "Installing Maven ${MAVEN_VERSION} on ${TRAVIS_OS_NAME}"
 MAVEN_INSTALL_FOLDER="${HOME}"
 echo "Creating ${MAVEN_INSTALL_FOLDER}"
 mkdir -p $MAVEN_INSTALL_FOLDER
 MAVEN_TARBALL="apache-maven-${MAVEN_VERSION}-bin.tar.gz"
-MAVEN_DOWNLOAD_URL="https://archive.apache.org/dist/maven/maven-3/${MAVEN_VERSION}/binaries/${MAVEN_TARBALL}"
+MAVEN_DOWNLOAD_URL="${MIRROR}${MAVEN_VERSION}/binaries/${MAVEN_TARBALL}"
 echo "Downloading Maven ${MAVEN_VERSION} from ${MAVEN_DOWNLOAD_URL} into ${MAVEN_INSTALL_FOLDER}"
 MAVEN_INSTALLER_LOCATION="${MAVEN_INSTALL_FOLDER}/${MAVEN_TARBALL}"
 curl -sL $MAVEN_DOWNLOAD_URL --output $MAVEN_INSTALLER_LOCATION
