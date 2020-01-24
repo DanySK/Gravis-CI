9961757a8fe0f470eea3178787e9c44e79acf6e5
Danilo Pianini <danilo.pianini@unibo.it>
2020-01-24 21:42:00 +0100

    Update .install-python.sh

--- .install-python.sh
+++ .install-python.sh
@@ -2,12 +2,7 @@
 # This script requires pyenv to be setup.
 # The PYTHON environment variable must be set to the desired Python version, or the latest stable will be used
 set -e
-LATEST_PYTHON_STABLE=$(pyenv install -l | awk '{$1=$1};1' | sort -r | grep -E '^([0-9]*\.)*[0-9]*$' -m 1)
-echo "The latest version available on pyenv is $LATEST_PYTHON_STABLE"
-PYTHON="${PYTHON:-$LATEST_PYTHON_STABLE}"
-echo "Python version selected: $PYTHON. Installing..."
-pyenv install --skip-existing $PYTHON
-echo "...running MacOS X fix..."
+echo "Running pyenv post-install fix"
 echo "Setting PYENV_ROOT to $HOME/.pyenv"
 export PYENV_ROOT="$HOME/.pyenv"
 echo "PYENV_ROOT is now $PYENV_ROOT"
@@ -17,6 +12,12 @@ echo "PATH is now $PATH"
 echo "Initialize pyenv shims"
 eval "$(pyenv init -)"
 echo "PATH is now $PATH"
+LATEST_PYTHON_STABLE=$(pyenv install -l | awk '{$1=$1};1' | sort -r | grep -E '^([0-9]*\.)*[0-9]*$' -m 1)
+echo "Done. Now installing Python."
+echo "The latest version available on pyenv is $LATEST_PYTHON_STABLE"
+PYTHON="${PYTHON:-$LATEST_PYTHON_STABLE}"
+echo "Python version selected: $PYTHON. Installing..."
+pyenv install --skip-existing $PYTHON
 echo "...done. Setting global to ${PYTHON}..."
 pyenv global $PYTHON
 echo "...done. Checking version."
