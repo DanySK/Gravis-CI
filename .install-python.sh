53be22ac10162ff3029e421910f9b55d96baa3d9
Danilo Pianini <danilo.pianini@unibo.it>
2020-01-24 11:54:54 +0100

    Create .install-python.sh

---
+++ .install-python.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# This script requires pyenv to be setup.
+# The PYTHON environment variable must be set to the desired Python version, or the latest stable will be used
+set -e
+LATEST_PYTHON_STABLE=$(pyenv install -l | awk '{$1=$1};1' | sort -r | grep -E '^([0-9]*\.)*[0-9]*$' -m 1)
+echo "The latest version available on pyenv is $LATEST_PYTHON_STABLE"
+PYTHON="${PYTHON:-$LATEST_PYTHON_STABLE}"
+echo "Python version selected: $PYTHON"
+echo "Installing Python $PYTHON..."
+pyenv install --skip-existing $PYTHON
+echo "...installation completed. Setting local version to $PYTHON..." 
+pyenv local $PYTHON
+echo "...done."
+set +e
