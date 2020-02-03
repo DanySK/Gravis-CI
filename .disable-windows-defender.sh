923a760fa7ca3a29d0754d1fa69de71df193c045
Danilo Pianini <danilo.pianini@gmail.com>
2020-02-03 21:57:52 +0100

    Notify win defender deprecation

--- .disable-windows-defender.sh
+++ .disable-windows-defender.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 # Windows Defender is now disabled by default.
+echo "Windows defender disabling script is deprecated and currently empty."
+echo "Travis now automatically disables Windows Defender"
+echo "Please remove this configuration from your build, it will get dropped in future"
