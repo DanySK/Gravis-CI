c2f0dcc62433cb761b081e4d5c116d25902788d8
Danilo Pianini <danilo.pianini@gmail.com>
2020-02-03 21:23:04 +0100

    This script is no longer useful, Windows Defender is disabled by default

--- .disable-windows-defender.sh
+++ .disable-windows-defender.sh
@@ -1,18 +1,2 @@
 #!/bin/bash
-# Note: I'm not sure this works.
-if [ $TRAVIS_OS_NAME = 'windows' ]
-then
-  echo "This is an attempt at disabling Windows Defender. I'm not sure it works."
-  export PROJECTDIR=$(pwd)
-  export TEMPDIR=$LOCALAPPDATA\\Temp
-  powershell Add-MpPreference -ExclusionPath ${PROJECTDIR}
-  powershell Add-MpPreference -ExclusionPath ${TEMPDIR}
-  powershell Add-MpPreference -ExclusionPath ${HOME}
-  echo "DisableArchiveScanning..."
-  powershell Start-Process -PassThru -Wait PowerShell -ArgumentList "'-Command Set-MpPreference -DisableArchiveScanning \$true'"
-  echo "DisableBehaviorMonitoring..."
-  powershell Start-Process -PassThru -Wait PowerShell -ArgumentList "'-Command Set-MpPreference -DisableBehaviorMonitoring \$true'"
-  echo "DisableRealtimeMonitoring..."
-  powershell Start-Process -PassThru -Wait PowerShell -ArgumentList "'-Command Set-MpPreference -DisableRealtimeMonitoring \$true'"
-  powershell -ExecutionPolicy Bypass Get-MpPreference
-fi
+# Windows Defender is now disabled by default.
